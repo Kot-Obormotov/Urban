ee09631b23e6876c0bf1f09c08d5fa6f389ca1ba
Max Vakhnin <maks-vakhnin@rambler.ru>
2025-01-03 13:56:18 +0300

    module_7_5.py
    #module_7_5 "Файлы в операционной системе"

--- module_7_5.py
+++ module_7_5.py
@@ -12,19 +12,27 @@ from datetime import datetime
     Используйте os.path.dirname для получения родительской директории файла.
 """
 
-print()
-os.chdir(os.getcwd())
-print(os.getcwd())
+print() #Просто разделение, чтобы было видно следующие данные
+os.chdir(os.getcwd()) #фиксируем директорию, чтобы не работать со всей файловой системой
+
+#формируем списки файлов, директорий и корней файлов потому что так в тз к задаче
 for root, dirs, files in os.walk(os.getcwd()):
     files = [f for f in os.listdir() if os.path.isfile(f)]
     dirs = [d for d in os.listdir() if os.path.isdir(d)]
-    root = [file for file in files if os.path.dirname(file)]
+    root = [file for file in files if os.path.abspath(file)]
 
+#можно было коррелировать данные между root, files и dirs,
+#но в задаче стоит работа с модулем os так что делаем проход только по именам файлов
 for file in files:
+    #определяем абсолютный путь до файла (в задаче не сказано, что нужна относительная)
     filepath = os.path.abspath(file)
+    # определяем время создания файла и форматируем его в удобоваримый формат
     filetime = datetime.fromtimestamp(os.path.getatime(file)).strftime("%d.%m.%Y %H:%M")
+    # определяем время изменения файла и форматируем его в удобоваримый формат
     formatted_time = datetime.fromtimestamp(os.path.getmtime(file)).strftime("%d.%m.%Y %H:%M")
+    # определяем размер файла в байтах
     filesize = os.path.getsize(file)
+    # #определяем абсолютный путь родительской директории файла (в задаче не сказано, что нужна относительная)
     parent_dir = os.path.abspath(os.path.join(filepath, os.pardir))
     print(
         f'Обнаружен файл: {file}, '
