ccfa633418c16cd3114bed7f76543360f8621865
Max Vakhnin <maks-vakhnin@rambler.ru>
2025-01-09 18:28:39 +0300

    module_8_1.py
    #module_8_1 "Try и Except"

--- module_8_1.py
+++ module_8_1.py
@@ -1,9 +1,16 @@
 #module_8_1 "Try и Except"
 
+"""
+add_everything_up, будет складывать числа(int, float) и строки(str)
+"""
+
+
 def add_everything_up(a, b):
     try:
-        str_ = round(a+b, 3)
+        #пробуем сложить и округлить числа (костыль - версия pycharm выдаёт сумму в третьем тесте 130.45600000000002)
+        str_ = a+b
     except TypeError:
+        #если нет возможности, то переводим в строковое и соединяем
         str_ = str(a)+str(b)
     return str_
 
